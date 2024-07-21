f7ae9bb117b15fa0d94c7db08bbfadcf25395ca0
Dominik Jankowski <dominik.jankowski@outlook.com>
2024-07-21 22:52:38 +0200

    feat: added variables, added tests for each account type log in

---
+++ cypress/integration/loginProblem-Test.spec.js
@@ -0,0 +1,18 @@
+/// <reference types = "cypress" />
+import loginPage from "../support/page-object/loginPage"
+import mainPage from "../support/page-object/mainPage"
+
+describe("Problem user log in and assertions on the login page:", () => {
+
+    it("Log in problem user:", () => {
+        cy.goToUrl()
+        cy.loginProblem()
+        cy.wait(5000)
+    })
+
+    it("Assert if you are on the main page:", () => {
+        mainPage.redRobot.should("be.visible") //check if the red robot icon is visible
+        mainPage.swagLabsLogo.should("be.visible") //check of the SwagLabs logo is visible
+        loginPage.btnLogin.should("not.exist") //check if the login button from the login page is not present
+    })
+})
